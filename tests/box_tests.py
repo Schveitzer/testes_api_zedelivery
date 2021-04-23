bbd180f1756492c54554d8961d3c4100abb6085e
alan <alan@rz2.com.br>
2021-04-23 10:44:06 -0300

    Refatora a forma como são passados os parâmetros

--- tests/box_tests.py
+++ tests/box_tests.py
@@ -9,27 +9,27 @@ from helpers.validate_json_schema import validate_json
 
 @allure.suite('Validações nos endpoints box da api pública openweathermap ')
 class BoxTests:
-    # A implementação dos métodos get_base_url, get_appid que são passados como fixtures podem ser vistas no arquivo conftest.py na raiz do projeto.
+
     @pytest.fixture(autouse=True)
     def setup(self, get_base_url, get_appid):
         self.base_url = get_base_url
-        self.appid = '&appid='+get_appid
+        self.appid = get_appid
         self.endpoint = 'data/2.5/box/city'
         self.url = str(self.base_url + self.endpoint)
 
     @allure.title('GET BOX - Valida se o status code está retornando corretamente quando efetuado um get com dados válidos')
     @allure.severity(allure.severity_level.NORMAL)
     def box_get_pela_cidade_test(self):
-        parametros = '?bbox=12,32,15,37,10'+self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'bbox': '12,32,15,37,10', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
 
         assert response.status_code == HTTPStatus.OK
 
     @allure.title('GET BOX - Valida o contrato da api para um get informando coordenadas corretas')
     @allure.severity(allure.severity_level.NORMAL)
     def box_get_contract_test(self):
-        parametros = '?bbox=12,32,15,37,10'+self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'bbox': '12,32,15,37,10', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
         base_schema = read_test_data_from_json('schema_box_cordenadas.json')
         response_body = response.json()
 
@@ -39,8 +39,8 @@ class BoxTests:
     @allure.title('GET WEATHER - Valida o código de retorno quando efetuado um get com uma área maior que a permitida')
     @allure.severity(allure.severity_level.NORMAL)
     def box_get_retorno_test(self):
-        parametros = '?bbox=12,32,15o,37,10'+self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'bbox': '12,32,15o,37,10', 'appid': {self.appid}}
+        response = requests.get(self.url, parametros)
         response_body = response.json()
 
         assert response.status_code == HTTPStatus.BAD_REQUEST
--- tests/weather_tests.py
+++ tests/weather_tests.py
@@ -9,52 +9,51 @@ from helpers.validate_json_schema import validate_json
 
 @allure.suite('Validações nos endpoints weather da api pública openweathermap ')
 class WeatherTests:
-    
-    # A implementação dos métodos get_base_url, get_appid que são passados como fixtures podem ser vistas e editadas no arquivo conftest.py na raiz do projeto.
+
     @pytest.fixture(autouse=True)
     def setup(self, get_base_url, get_appid):
         self.base_url = get_base_url
-        self.appid = '&appid='+get_appid
+        self.appid = get_appid
         self.endpoint = 'data/2.5/weather'
         self.url = str(self.base_url + self.endpoint)
 
     @allure.title('GET WEATHER - Valida se o status code está retornando corretamente quando efetuado um get pelo nome da cidade')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_pela_cidade_test(self):
-        parametros = '?q=London,uk'+self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'q': 'London,uk', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
 
         assert response.status_code == HTTPStatus.OK
 
     @allure.title('GET WEATHER - Valida se o status code está retornando corretamente quando efetuado um get pelo ID da cidade')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_pelo_id_test(self):
-        parametros = '?id=2172797' + self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'id': '2172797', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
 
         assert response.status_code == HTTPStatus.OK
 
     @allure.title('GET WEATHER - Valida se o status code está retornando corretamente quando efetuado um get pelas coordenadas da cidade')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_pelas_coordenadas_test(self):
-        parametros = '?lat=51.5085&lon=-0.1257' + self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'lat': '51.5085', 'lon': '0.1257', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
 
         assert response.status_code == HTTPStatus.OK
 
     @allure.title('GET WEATHER - Valida se o status code está retornando corretamente quando efetuado um get pelo código postal da cidade')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_pelo_cod_postal_test(self):
-        parametros = '?zip=94040' + self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'zip': '94040', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
 
         assert response.status_code == HTTPStatus.OK
 
     @allure.title('GET WEATHER - Valida se as cordenadas tazidas no retorno estão corretas')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_coord_test(self):
-        parametros = '?q=London,uk' + self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'q': 'London,uk', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
         response_body = response.json()
 
         assert response.status_code == HTTPStatus.OK
@@ -64,8 +63,8 @@ class WeatherTests:
     @allure.title('GET WEATHER - Valida o contrato da api para um get informando uma cidade')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_contract_test(self):
-        parametros = '?q=London,uk' + self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'q': 'London,uk', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
         base_schema = read_test_data_from_json('schema_weather_cidade.json')
         response_body = response.json()
 
@@ -75,8 +74,8 @@ class WeatherTests:
     @allure.title('GET WEATHER - Valida o código e o retorno quando efetuado um get sem informar nenhum parâmetro')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_retorno_mensagem_test(self):
-        parametros = '?'+self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'?': '', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
         response_body = response.json()
 
         assert response.status_code == HTTPStatus.BAD_REQUEST
@@ -86,8 +85,8 @@ class WeatherTests:
     @allure.title('GET WEATHER - Valida o código e o retorno quando efetuado um get informando um appid inválido')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_appid_invalido_test(self):
-        parametros = '?q=London,uk&appid=45345345345345'
-        response = requests.get(self.url + parametros)
+        parametros = {'q': 'London,uk&', 'appid': '45345345345345'}
+        response = requests.get(self.url, parametros)
         response_body = response.json()
 
         assert response.status_code == HTTPStatus.UNAUTHORIZED
@@ -97,8 +96,8 @@ class WeatherTests:
     @allure.title('GET WEATHER - Valida o código e o retorno quando efetuado um get informando uma cidade que não existe')
     @allure.severity(allure.severity_level.NORMAL)
     def weather_get_cidade_inexistente_test(self):
-        parametros = '?q=Londo,uk'+self.appid
-        response = requests.get(self.url + parametros)
+        parametros = {'q': 'Londo,uk', 'appid': self.appid}
+        response = requests.get(self.url, parametros)
         response_body = response.json()
 
         assert response.status_code == HTTPStatus.NOT_FOUND
